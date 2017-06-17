60e2644ec48a0bd7b79deb0d475080d02695c116
Dion Larson <me@dionlarson.com>
2017-06-17 15:43:32 -0400

    Fixes #14

--- Functions.playground/Pages/01-Becoming-Functional.xcplaygroundpage/Contents.swift
+++ Functions.playground/Pages/01-Becoming-Functional.xcplaygroundpage/Contents.swift
@@ -8,7 +8,7 @@
  ## Let's call a function!
 
  We're going to jump right into things so you can call your first function. Enter the following line of code and watch the visualization to see what happens!
- 
+
     moveFifty()
 
 */
@@ -26,14 +26,14 @@
     functionName()
 
  In this case, you called a function named `moveFifty`. What happened in the visualization? You should have seen the triangle move `50 units` and leave behind a line.
- 
- It's important to keep in mind that functions (like variables) must be defined before they are called. We have already defined the `rotateNinety()` function for you in another file. `rotateNinety()` will work anywhere but you cannot just call `moveThirty()` and expect it to work. A `moveThirty` has not been defined yet. We'll show you how to define your own functions soon but let's focus on calling them for now.
- 
+
+ It's important to keep in mind that functions (like variables) must be defined before they are called. We have already defined the `moveFifty()` function for you in another file. `moveFifty()` will work anywhere but you cannot just call `moveThirty()` and expect it to work. A `moveThirty` has not been defined yet. We'll show you how to define your own functions soon but let's focus on calling them for now.
+
  - experiment: What happens if you call the `moveFifty` function again in the space below? What happens if you call it a third time?
- 
- 
+
+
  - important: _Make sure each function call is on a separate line and don't forget the parentheses!_
- 
+
  */
 
 
@@ -42,9 +42,9 @@
 
 
 /*:
- 
+
  ## What happened?
- 
+
  Every time you call `moveFifty` the function's code is executed another time. With 3 calls in a row to `moveFifty` the triangle should move `150 units` in a straight line and leave a path behind it.
 
 */
